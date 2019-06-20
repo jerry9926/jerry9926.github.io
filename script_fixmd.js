0ed601e0ff4f38165925929978095b28679584a1
kuler <kulerhuang@macbook.com>
2019-06-21 00:32:31 +0800

    文章[2019-05-15-eslint+husky实现代码校验]编辑中

--- script_fixmd.js
+++ script_fixmd.js
@@ -1,87 +1,78 @@
-var fs = require('fs');
-var path = require('path');
-var readline = require('linebyline');
+const fs = require('fs')
+const path = require('path')
+const readline = require('linebyline')
 
-var DIR = '_posts';
-// 2016-02-19-webpack入门笔记;
+const DIR = '_posts'
+// 2016-02-19-webpack入门笔记
 // 2017-03-10-jekyll + github page标签页404
 // 2017-03-07-原生javascript API实现HTML5浏览器全屏
 // 2017-09-5-在express中使用markdown
 // 2017-09-14-Babel小记
 // 2017-10-10-扩展echarts图表类型
 // 2017-07-05-Swagger+Node搭建API文档系统
-var FILE_PATH = '2017-07-05-Swagger+Node搭建API文档系统';
+// 2019-05-15-eslint+husky实现代码校验
+const FILE_PATH = '2019-05-15-eslint+husky实现代码校验'
 
-var SUFFIX = '.markdown';
+const SUFFIX = '.markdown'
 
 fs.readdir(DIR, function(err, files) {
-	for (var i = 0; i < files.length; i++) {
-		var file = files[i];
-
-		// console.log(file);
-		// console.log(FILE_PATH);
-		// console.log(file.indexOf(FILE_PATH));
-
+	files.map((file) => {
 		if (file.indexOf(FILE_PATH + SUFFIX) === 0) {
-			var filePath =  path.join(DIR, file);
-			// var isOpen = true;
-			var isJsEnd = false;
-			var isCodeEnd = true;
+			const filePath =  path.join(DIR, file)
+			const fileBaseName = path.basename(file, SUFFIX)
+			const filePathNew = path.join(DIR, fileBaseName + '.new') + SUFFIX
+			let isHTMLOpen = false
+			let isJsOpen = false
 
-			var fileBaseName = path.basename(file, SUFFIX);
-			var filePathNew = path.join(DIR, fileBaseName + '.new') + SUFFIX;
+			console.log(filePath)
+			console.log(filePathNew)
 
-			console.log(filePath);
-			console.log(filePathNew);
-
-			var ws = fs.createWriteStream(filePathNew);
-			var rl = readline(filePath);
+			const ws = fs.createWriteStream(filePathNew)
+			const rl = readline(filePath)
 
 			rl.on('line', function(line, lineCount, byteCount) {
 
-// console.log('line - ' + lineCount + ', ' + line);
+			// console.log('line - ' + lineCount + ', ' + line)
 
 				// ## ### 前面增加 换行
 				if (line.match(/(^#{2,3})(?!#)/)) {
-					console.log('line - ' + lineCount + ', ' + line);
-					line = '<br>' + '\n' + line;
+					console.log('line - ' + lineCount + ', ' + line)
+					line = '<br>' + '\n' + line
 				}
 
 				// 单行是引用链接时增加 换行
 				if (line.match(/^\[.+\]\(.+\)$/)) {
-					console.log('line - ' + lineCount + ', ' + line);
-					line = '<br>' + '\n' + line;					
+					console.log('line - ' + lineCount + ', ' + line)
+					line = '<br>' + '\n' + line					
 				}
 
 				// js的代码高亮
 				if (line.match(/^```js$/)) {
-					line = '{% highlight javascript %}';
-					isJsEnd = true;
-				} else if (line.match(/^```$/) && isJsEnd) {
-					line = '{% endhighlight %}';
-					isJsEnd = false;					
+					line = '{% highlight javascript %}'
+					isJsOpen = true
+				} else if (line.match(/^```$/) && isJsOpen) {
+					line = '{% endhighlight %}'
+					isJsOpen = false					
 				}
 
-				// if (line.match(/^```$/)) {
-				// 	if (isOpen) {
-				// 		line = '{% highlight javascript %}';
-				// 		isOpen = false;
-				// 	} else {
-				// 		line = '{% endhighlight %}';
-				// 		isOpen = true;
-				// 	}
-				// }
+				// html的代码高亮
+				if (line.match(/^```html$/)) {
+					line = '{% highlight html %}'
+					isHTMLOpen = true
+				} else if (line.match(/^```$/) && isHTMLOpen) {
+					line = '{% endhighlight %}'
+					isHTMLOpen = false					
+				}
 
-				ws.write(line + '\n');
+				ws.write(line + '\n')
 
 			}).on('error', function(e) {
-				console.error(e);
+				console.error(e)
 			}).on('close', function() {
-				ws.end();
-				console.log('file close');
+				ws.end()
+				console.log('file close')
 			})
 		}
-
-	};
-});
+	})
+})
 
